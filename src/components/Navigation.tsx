6b63387987dcea9bd198ac47bcc7b2733a1dd6e5
Manu Morante <manu@estadologico.com>
2025-05-15 12:22:06 +0200

    use Navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router'
 
 export default function Navigation() {
   return (
-    <div>
+    <div className="Navigation space-x-4 py-4">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/shows">Shows</NavLink>
       <NavLink to="/shows/id-example">Shows ID</NavLink>
