feb517ea3096e5d6775f9f78f8bd65846907b2a7
Manu Morante <manu@estadologico.com>
2025-05-15 10:02:46 +0200

    add API getShows

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,1 +1,13 @@
-// https://www.tvmaze.com/api
+import { Show } from '@/types'
+
+const API_BASE_URL = 'https://api.tvmaze.com'
+
+export async function getShows(): Promise<Show[]> {
+  const response = await fetch(`${API_BASE_URL}/shows`)
+
+  if (!response.ok) {
+    throw new Error('[getShows] Error loading shows')
+  }
+
+  return response.json()
+}
