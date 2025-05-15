91a61602fc8e23f3eb7a0ab4ec10baec94b80ab3
Manu Morante <manu@estadologico.com>
2025-05-15 20:45:47 +0200

    add clsx and active in nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,36 @@
+import { HomeIcon, FilmIcon, HeartIcon } from '@heroicons/react/24/outline'
+import cx from 'clsx'
+
 import { NavLink } from 'react-router'
 
 export default function Header() {
+  const styles = {
+    item: 'flex items-center gap-1.5 text-slate-500',
+    active: 'text-slate-950',
+  }
+
   return (
-    <header className="sticky top-0 z-10 bg-slate-950 text-slate-200 shadow-xs">
+    <header className="sticky top-0 z-10 bg-slate-50 shadow-xs">
       <div className="container mx-auto max-w-5xl">
-        <nav className="flex gap-6 py-5">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/shows">Shows</NavLink>
-          <NavLink to="/favorites">Favorites</NavLink>
+        <nav className="flex gap-8 py-5">
+          <NavLink
+            className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
+            to="/">
+            <HomeIcon className="h-5 w-5" />
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
+            to="/shows">
+            <FilmIcon className="h-5 w-5" />
+            Shows
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
+            to="/favorites">
+            <HeartIcon className="h-5 w-5" />
+            Favorites
+          </NavLink>
         </nav>
       </div>
     </header>
