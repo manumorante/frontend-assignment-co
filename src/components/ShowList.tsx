66ea4a45c07b0cb8e0801c3ac30bd2b409f60947
Manu Morante <manu@estadologico.com>
2025-05-16 08:13:24 +0200

    refactor: move data fetching from ShowList component to parent pages for reusability

--- src/components/ShowList.tsx
+++ src/components/ShowList.tsx
@@ -1,26 +1,12 @@
-import { ErrorMessage, Loading, ShowCard } from '@/components'
-import { useShowList } from '@/hooks/useShowList'
+import { ShowCard } from '@/components'
+import { Show } from '@/types'
 
-export default function ShowList() {
-  const { shows, isLoading, error, fetchNextPage, hasNextPage } = useShowList()
-
-  if (isLoading) return <Loading />
-  if (error) return <ErrorMessage message={error.message} />
-  if (shows.length === 0) return <ErrorMessage message="No results" />
-
+export default function ShowList({ shows }: { shows: Show[] }) {
   return (
-    <div className="ShowList w-full">
-      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
-        {shows.map((show) => (
-          <ShowCard key={show.id} show={show} />
-        ))}
-      </div>
-
-      {hasNextPage && (
-        <button className="button secondary my-4 w-full" onClick={() => fetchNextPage()}>
-          Load More Shows
-        </button>
-      )}
+    <div className="ShowList grid w-full grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+      {shows.map((show) => (
+        <ShowCard key={show.id} show={show} />
+      ))}
     </div>
   )
 }
--- src/pages/ShowListPage.tsx
+++ src/pages/ShowListPage.tsx
@@ -1,9 +1,22 @@
-import ShowList from '@/components/ShowList'
+import { ErrorMessage, Loading, ShowList } from '@/components'
+import { useShowList } from '@/hooks/useShowList'
 
 export default function ShowListPage() {
+  const { shows, isLoading, error, fetchNextPage, hasNextPage } = useShowList()
+
+  if (isLoading) return <Loading />
+  if (error) return <ErrorMessage message={error.message} />
+  if (shows.length === 0) return <ErrorMessage message="There are no shows available to display." />
+
   return (
-    <div className="M">
-      <ShowList />
+    <div className="ShowListPage">
+      <ShowList shows={shows} />
+
+      {hasNextPage && (
+        <button className="button secondary my-4 w-full" onClick={() => fetchNextPage()}>
+          Load More Shows
+        </button>
+      )}
     </div>
   )
 }
