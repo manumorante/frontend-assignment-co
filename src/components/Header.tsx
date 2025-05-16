c47c49cd1e1eb1f34a60003ef6843bc873983494
Manu Morante <manu@estadologico.com>
2025-05-16 15:33:14 +0200

    style: update Header component styles for improved layout and visual consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,49 @@
-import { HomeIcon, FilmIcon, StarIcon } from '@heroicons/react/24/outline'
 import cx from 'clsx'
-
 import { NavLink } from 'react-router'
 
 export default function Header() {
   const styles = {
-    item: 'flex items-center gap-1.5 text-zinc-500',
-    active: 'text-zinc-950',
+    header: cx(
+      // Positioning & Layer
+      'sticky top-4 z-30 mx-auto mb-4',
+
+      // Sizing & Layout
+      'max-w-2xl px-2 sm:px-4',
+
+      // Visuals
+      'rounded-2xl border border-transparent bg-white shadow-lg',
+
+      // Effects
+      'transition-[box-shadow,background-color] duration-300',
+    ),
+    item: cx(
+      'flex items-center gap-1.5',
+      'px-3 py-2 rounded-lg',
+      'text-zinc-500',
+      'transition-colors hover:bg-zinc-100 duration-400',
+    ),
+    active: 'bg-zinc-100 text-zinc-950',
   }
 
   return (
-    <header className="sticky top-0 z-10 bg-zinc-50 shadow-xs">
-      <div className="main-container">
-        <nav className="flex gap-8 py-5">
-          <NavLink
-            className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
-            to="/">
-            <HomeIcon className="h-5 w-5" />
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
-            to="/shows">
-            <FilmIcon className="h-5 w-5" />
-            Shows
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
-            to="/favorites">
-            <StarIcon className="h-5 w-5" />
-            Favorites
-          </NavLink>
-        </nav>
-      </div>
+    <header className={styles.header}>
+      <nav className="flex gap-4 py-1 sm:gap-6">
+        <NavLink
+          className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
+          to="/">
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
+          to="/shows">
+          TV Shows
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? cx(styles.item, styles.active) : styles.item)}
+          to="/favorites">
+          Favorites
+        </NavLink>
+      </nav>
     </header>
   )
 }
